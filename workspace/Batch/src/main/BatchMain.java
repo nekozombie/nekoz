7e291970f5fb5b3153154f7810862c47cb53910f
h.aoki <ghostiscat@gmail.com>
2014-10-13 22:35:10 +0900

    今度こそBatchクラスをテストコミット

---
+++ workspace/Batch/src/main/BatchMain.java
@@ -0,0 +1,15 @@
+package main;
+
+import job.ExcelControll;
+
+public class BatchMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		new ExcelControll().execute();
+	}
+
+}
